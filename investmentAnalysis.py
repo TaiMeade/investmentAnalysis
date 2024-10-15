53b36e3f682db360d714f45cf4bbba834a75ad4d
TaiMeade <ttmeade@radford.edu>
2024-10-15 17:26:54 -0400

    probably going with yfinance instead of morningstar

--- investmentAnalysis.py
+++ investmentAnalysis.py
@@ -2,11 +2,14 @@ import streamlit as st
 import morningstar_data as md
 import os
 import pandas as pd
-import requests
+# import requests
 import json
 from IPython.display import display
 # from xbrl import XBRLParser
+import yfinance as yf
+import pprint
 
+# This is a list of searches offered by Morningstar...used this list to assist in getting ids for each datapoint since I could not find them online ANYWHERE
 # 0   "0218,0020,"                                           Snapshot
 # 1   "0218-1203,"  Sustainable Investing: Low Carbon Transition M...
 # 2   "0218-0497,"     Sustainable Investing: Apply Exclusions (Fund)
@@ -96,7 +99,7 @@ from IPython.display import display
 # 86  "0218-0103,"                        Fees Schedule and Breakdown
 # 87  0218-0440,"                                         Risk Model
 
-os.environ['MD_AUTH_TOKEN'] = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1EY3hOemRHTnpGRFJrSTRPRGswTmtaRU1FSkdOekl5TXpORFJrUTROemd6TWtOR016bEdOdyJ9.eyJodHRwczovL21vcm5pbmdzdGFyLmNvbS9lbWFpbCI6ImludmVzdEByYWRmb3JkLmVkdSIsImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL3JvbGUiOlsiUGVyc29uYS5EaXJlY3RGb3JBc3NldE1hbmFnZW1lbnQiLCJFbmFibGVkIEFuYWx5dGljcyBMYWIgRGVsaXZlcnkgTm90ZWJvb2tzIiwiRGlzYWJsZSBEZWZpbmVkIENvbnRyaWJ1dGlvbiBQbGFucyIsIlBvcnRmb2xpbyBBbmFseXNpcyBVc2VyIl0sImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL2NvbXBhbnlfaWQiOiIxMWZjMjA1MC01YmFhLTQzOTYtODI3ZS0xNzRlNzk4MDJlODkiLCJodHRwczovL21vcm5pbmdzdGFyLmNvbS9pbnRlcm5hbF9jb21wYW55X2lkIjoiQ2xpZW50MCIsImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL2RhdGFfcm9sZSI6W10sImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL2xlZ2FjeV9jb21wYW55X2lkIjoiMTFmYzIwNTAtNWJhYS00Mzk2LTgyN2UtMTc0ZTc5ODAyZTg5IiwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vcm9sZV9pZCI6WyI3OGJhMWFlNy0xZWUzLTQ0YTAtYTAxOC0wOGM1NThmZWNmMTciLCI4MjYyOWNkMC1kZjgwLTRlNWMtYjNiYS02YmQyNWU5MzBhNDIiLCJkYzMxN2Q5OC0xMTAwLTQyM2YtOTUzZi1mZjRkYjc4MzUwMTgiXSwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vcHJvZHVjdCI6WyJESVJFQ1QiLCJQUyJdLCJodHRwczovL21vcm5pbmdzdGFyLmNvbS9jb21wYW55IjpbeyJpZCI6IjExZmMyMDUwLTViYWEtNDM5Ni04MjdlLTE3NGU3OTgwMmU4OSIsInByb2R1Y3QiOiJESVJFQ1QifV0sImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL21zdGFyX2lkIjoiQUU1RTJFN0YtNUE1QS00RUQ5LUFEQUQtNTE4MDUzQTBDQ0NBIiwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJodHRwczovL21vcm5pbmdzdGFyLmNvbS9wYXNzd29yZENoYW5nZVJlcXVpcmVkIjpmYWxzZSwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vdWltX3JvbGVzIjoiRUFNUyxNRF9NRU1CRVJfMV8xLERPVF9DT01fRlJFRSxESVJFQ1QiLCJpc3MiOiJodHRwczovL2xvZ2luLXByb2QubW9ybmluZ3N0YXIuY29tLyIsInN1YiI6ImF1dGgwfEFFNUUyRTdGLTVBNUEtNEVEOS1BREFELTUxODA1M0EwQ0NDQSIsImF1ZCI6WyJodHRwczovL3VpbS1wcm9kLm1vcm5pbmdzdGFyLmF1dGgwLmNvbS9hcGkvdjIvIiwiaHR0cHM6Ly91aW0tcHJvZC5tb3JuaW5nc3Rhci5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzI4OTI1NDUxLCJleHAiOjE3MjkwMTE4NTEsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgb2ZmbGluZV9hY2Nlc3MiLCJhenAiOiJDaGVLTTR1ajhqUFQ2MGFVMkk0Y1BsSDhyREtkT3NaZCJ9.FNq38iFc_cgEZT-LTa2N4nodfpZNZcz9MyAkYiwRUGBdqV-9oA7TpUA3CUSseR4jM6S-HX5ePi5ye9ieMetpr2uRI3-0KRLjZSq-BaXP9ldzjC7x_qzGHFFSPsodHfTKYpzrICJ6shU1yGusMpCX80wEgIYnywdk46jUEWStqcggqPAM9m04Vlaltcl48f1Kp6fprghGQe0ACr4Jw52Rtcroji73fRcw8wselCSC12JF2mEQqT-8yXpfeNSPYtenUr7jdMEtbrzFKYxVOWcKM2RiRmo3ktuDSn7RE2qc-XEY7yqZ2GAM9JodnIB5XRFP_lclPGVF_y4yEy0Payvt7Q"
+os.environ['MD_AUTH_TOKEN'] = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1EY3hOemRHTnpGRFJrSTRPRGswTmtaRU1FSkdOekl5TXpORFJrUTROemd6TWtOR016bEdOdyJ9.eyJodHRwczovL21vcm5pbmdzdGFyLmNvbS9lbWFpbCI6ImludmVzdEByYWRmb3JkLmVkdSIsImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL3JvbGUiOlsiUGVyc29uYS5EaXJlY3RGb3JBc3NldE1hbmFnZW1lbnQiLCJFbmFibGVkIEFuYWx5dGljcyBMYWIgRGVsaXZlcnkgTm90ZWJvb2tzIiwiRGlzYWJsZSBEZWZpbmVkIENvbnRyaWJ1dGlvbiBQbGFucyIsIlBvcnRmb2xpbyBBbmFseXNpcyBVc2VyIl0sImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL2NvbXBhbnlfaWQiOiIxMWZjMjA1MC01YmFhLTQzOTYtODI3ZS0xNzRlNzk4MDJlODkiLCJodHRwczovL21vcm5pbmdzdGFyLmNvbS9pbnRlcm5hbF9jb21wYW55X2lkIjoiQ2xpZW50MCIsImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL2RhdGFfcm9sZSI6W10sImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL2xlZ2FjeV9jb21wYW55X2lkIjoiMTFmYzIwNTAtNWJhYS00Mzk2LTgyN2UtMTc0ZTc5ODAyZTg5IiwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vcm9sZV9pZCI6WyI3OGJhMWFlNy0xZWUzLTQ0YTAtYTAxOC0wOGM1NThmZWNmMTciLCI4MjYyOWNkMC1kZjgwLTRlNWMtYjNiYS02YmQyNWU5MzBhNDIiLCJkYzMxN2Q5OC0xMTAwLTQyM2YtOTUzZi1mZjRkYjc4MzUwMTgiXSwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vcHJvZHVjdCI6WyJESVJFQ1QiLCJQUyJdLCJodHRwczovL21vcm5pbmdzdGFyLmNvbS9jb21wYW55IjpbeyJpZCI6IjExZmMyMDUwLTViYWEtNDM5Ni04MjdlLTE3NGU3OTgwMmU4OSIsInByb2R1Y3QiOiJESVJFQ1QifV0sImh0dHBzOi8vbW9ybmluZ3N0YXIuY29tL21zdGFyX2lkIjoiQUU1RTJFN0YtNUE1QS00RUQ5LUFEQUQtNTE4MDUzQTBDQ0NBIiwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJodHRwczovL21vcm5pbmdzdGFyLmNvbS9wYXNzd29yZENoYW5nZVJlcXVpcmVkIjpmYWxzZSwiaHR0cHM6Ly9tb3JuaW5nc3Rhci5jb20vdWltX3JvbGVzIjoiRUFNUyxNRF9NRU1CRVJfMV8xLERPVF9DT01fRlJFRSxESVJFQ1QiLCJpc3MiOiJodHRwczovL2xvZ2luLXByb2QubW9ybmluZ3N0YXIuY29tLyIsInN1YiI6ImF1dGgwfEFFNUUyRTdGLTVBNUEtNEVEOS1BREFELTUxODA1M0EwQ0NDQSIsImF1ZCI6WyJodHRwczovL3VpbS1wcm9kLm1vcm5pbmdzdGFyLmF1dGgwLmNvbS9hcGkvdjIvIiwiaHR0cHM6Ly91aW0tcHJvZC5tb3JuaW5nc3Rhci5hdXRoMC5jb20vdXNlcmluZm8iXSwiaWF0IjoxNzI5MDIxMjM2LCJleHAiOjE3MjkxMDc2MzYsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgb2ZmbGluZV9hY2Nlc3MiLCJhenAiOiJDaGVLTTR1ajhqUFQ2MGFVMkk0Y1BsSDhyREtkT3NaZCJ9.tVXP0O4b_7ezBEvVp9yIaAitUbN0w6Nz8TLWUAixeVDhfv8RSgpWwPvGTnEkTrTHrQ7yu9uOka07Y7TsQBA8FUhJdayM4h7VrlN2Vfdax7tJv1-GbRuQLg7bXqcHQJsz9RIh0uSBKX9GSdykpsczSH0_5k_G35PjqGJDDlz5YybQGJaXd8ryenWDX-bab1XGAg8jBZGlGHOPHuYBGi01_z9IT98ha1GpegQZOXmAWjGV2D20rgpOp8Nlr7QJXWX1eG-O36320C8NK-55lpjXGKgebfk7-Ce1RQsCowTguaakErELaXG7hdcpXfN4uEVzqqbMORxG8noEoMnKhW9Dkg"
 
 # Display all rows and all columns of DataFrame objects when printing
 pd.set_option('display.max_columns',None)
@@ -108,7 +111,7 @@ pd.set_option('display.precision',3)
 # data = md.get_data("ANF")
 
 # test = md.direct.user_items.get_data_set_details("0218-0440")
-morningstar_data_sets = md.direct.get_morningstar_data_sets()
+# morningstar_data_sets = md.direct.get_morningstar_data_sets() REACTIVATE
 
 # 
 # list_of_tests = ["0218-0020","0218-1203","0218-0497","0218-0450","0218-0498","0218-0499","0218-0981","0218-0982","0218-0985","0218-0983","0218-0503","0218-0984","0218-0500","0218-0494","0218-1201","0218-0502","0218-0526","0218-0501","0218-0504","0218-0490","0218-0491","0218-0492","0218-0493","0218-0495","0218-0511","0218-0512","0218-0513","0218-0472","0218-0480","0218-0034","0218-0036","0218-0037","0218-0038","0218-0039","0218-0260","0218-0261","0218-0292","0218-0041","0218-0043","0218-0044","0218-0045","0593-0255","0218-0253","0218-0102","0218-0047","0218-0353","0218-0048","0218-0049","0218-0050","0218-0051","0218-0035","0218-0052","0218-0053","0218-0300","0218-0418","0218-0420","0218-0432","0218-0422","0218-0514","0218-0485","0218-0468","0218-0483","0218-0473","0218-0470","0218-0430","0218-0424","0218-0434","0218-0426","0218-0438","0218-0986","0218-0428","0218-0054","0218-0323","0218-0324","0218-0325","0218-0326","0218-0078","0218-0227","0218-0056","0218-0520","0218-0057","0218-0309","0218-0442","0218-0444","0218-0058"]
@@ -136,8 +139,8 @@ morningstar_data_sets = md.direct.get_morningstar_data_sets()
 
 
 # shows saved search criteria associated with our Morningstar account
-data = md.direct.user_items.get_search_criteria()
-
+# ourSavedSearches = md.direct.user_items.get_search_criteria() REACTIVATE
+# print(ourSavedSearches)
 # print(data)
 # print("\n---------------------------------------------------------\n")
 #
@@ -157,15 +160,34 @@ datapoint_ids = [
         "startDate": "2024-08-30",
         "endDate": "2024-10-30"
      }, # P/E
-    # {"datapointId": "HS05V"}, # P/B
-    # {"datapointId": "HS05U"}, # P/S
+    {
+        "datapointId": "HS05V",
+        "startDate": "2024-08-30",
+        "endDate": "2024-10-30"
+     }, # P/B
+    {
+        "datapointId": "HS05U",
+        "startDate": "2024-08-30",
+        "endDate": "2024-10-30"
+     }, # P/S
 ]
 
 # 17 is my search criteria...18 is from Fall 2023
-criteria = md.direct.user_items.get_search_criteria_conditions(search_criteria_id=data['id'][17])
+# criteriaNum = int(input('\nPlease enter the number of the saved criteria you would like to use: ')) REACTIVATE
+# REACTIVATE print("Search being conducted: " + ourSavedSearches['name'][criteriaNum - 2] + "\n") # we subtract 2 because the list we retrieve contains an extra entry and starts at index 0 rather than 1 like in Direct
 
-data = md.direct.get_investment_data(investments=criteria, data_points=datapoint_ids)
-display(data.to_string())
+# criteria = md.direct.user_items.get_search_criteria_conditions(search_criteria_id=ourSavedSearches['id'][criteriaNum - 2]) # we subtract 2 because the list we retrieve contains an extra entry and starts at index 0 rather than 1 like in Direct REACTIVATE
+
+# data = md.direct.get_investment_data(investments=criteria, data_points=datapoint_ids) REACTIVATE
+
+# write the results to a file (hoping this will improve readability)
+# f = open("searchResults.txt", "w") # using w will overwite whatever is already in the file...this is to prevent repeat information at the moment REACTIVATE
+
+# Condition to remove the rows/companies that result in NaN (Not a Number) for value criteria...TRY to remove them in future as this is NOT IMPLEMENTED yet
+# f.write(data.to_string()) REACTIVATE
+# f.close() REACTIVATE
+
+# display(data.to_string()) # this line is to print the information to the terminal in a somewhat structured form...not extremely structured because the terminal does not seem to support horizontal scrolling
 
 # abercrombie = md.direct.lookup.companies('Ford Motor')
 
@@ -199,4 +221,17 @@ display(data.to_string())
 
 # for fact in test2['facts']['us-gaap']:
 #     print(fact)
+def step_1():
+    userInput = input('Enter a ticker: ')
 
+    test = yf.Ticker(userInput).info
+
+    pprint.pprint(test)
+
+    # sharePrice / trailingEPS
+    priceToEarnings = test['currentPrice'] / (test['trailingEps'])
+
+    print('\n-----------------------------------------------------\nP/B Ratio: ' + str(test['priceToBook']) + "\nP/S Ratio: " + str(test['priceToSalesTrailing12Months']) + "\nP/E Ratio: " + str(priceToEarnings) + "\nEBITDA: " + str(test['ebitda']))
+
+step_1()
+step_1()
